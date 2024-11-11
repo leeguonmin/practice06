026964b9ae8b3b8b236ac64b771e8fa75d8cc18f
leegeonmin <l_lpp@naver.com>
2024-11-12 00:38:53 +0900

    집에서 커밋

--- src/com/javaex/ex04/UserApp.java
+++ src/com/javaex/ex04/UserApp.java
@@ -3,22 +3,71 @@ package com.javaex.ex04;
 public class UserApp {
 
 	public static void main(String[] args) {
-	
-		
-		int[][] two = new int[3][4];
-		
-		int sum = 0; 
-		for (int row = 0; row < table.length; ++row) { 	// 행 루프
-			for (int col = 0; col < table[row].length; ++col) {	// 열루프
-				int val = table[row][col];
-				
-				System.out.print(val + "\t");
-				sum += val; 		// 내부 cell 데이터 합산
-			}
 		
 		
 		
 		
+		
+		
+		
+		
+	}
+}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+/*
+		        // User 타입의 배열 선언 (Customer와 Employee 객체 모두 저장 가능)
+		        User[] users = new User[3];
+
+		        // 배열에 Customer와 Employee 객체를 추가
+		        users[0] = new Customer("jws", "1234", "정우성", 1000);
+		        users[1] = new Customer("yys", "3455", "이효리", 2000);
+		        users[2] = new Employee("master", "m7788", "운영자", 5000000);
+
+		        // 반복문을 통해 배열에 있는 객체들의 정보를 출력
+		        for (int i = 0; i < users.length; i++) { 
+		            users[i].show();  // 각 객체의 show() 메서드를 호출하여 정보 출력
+		        }
+
+		        System.out.println("운영자의 월급은 " + salary + "원 입니다.");
+		    }
+
+}
+*/		
+		
+		
+		
+		
+		
+		
+		
+		
+		//2.
+//		int[][] two = new int[3][4];
+//		
+//		int sum = 0; 
+//		for (int row = 0; row < table.length; ++row) { 	// 행 루프
+//			for (int col = 0; col < table[row].length; ++col) {	// 열루프
+//				int val = table[row][col];
+//				
+//				System.out.print(val + "\t");
+//				sum += val; 		// 내부 cell 데이터 합산
+//			}
+		
+		
+		
+		// 1. 
 //		Customer t = new Customer("Jws", "J1234", "정우성", 1000);
 //		t.show();
 //		
@@ -28,10 +77,13 @@ public class UserApp {
 //		Employee r = new Employee("master", "m7894", "운영자", 5000);
 //		r.show();
 
-	}
-
-}
-		
+//	}
+//
+//}
+//		
 
 // 배열로 방 3개  만들어서, 유저의 배열 3개짜리를 만들고 
 // 방마다 회원들 정도를 넣어 / 루프 돌아 / 
+
+// System.out.println("별도로 출력/합니다.");/
+// 별도로 출력할 내용 (운영자의 월급)
